6e4ef84c3b2957f9f1b1d08d35a045da150703c2
jjvessel <143030943+jjvessel@users.noreply.github.com>
2023-10-28 15:33:16 -0400

    Added 2 levels

--- Player/Player.gd
+++ Player/Player.gd
@@ -36,3 +36,10 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+
+
+func _on_exit_body_entered(body):
+	if body.name == "Player":
+		get_tree().change_scene_to_file("res://level_2.tscn")
